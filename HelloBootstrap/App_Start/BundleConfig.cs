03c7f2ce87bdd4f2878120f60db4727f423542dc
Johnny Diamond <johnathan_diamond@hotmail.com>
2018-09-13 23:10:00 +0100

    implementing LESS and bootstrap.

--- HelloBootstrap/App_Start/BundleConfig.cs
+++ HelloBootstrap/App_Start/BundleConfig.cs
@@ -27,7 +27,7 @@ namespace HelloBootstrap
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
